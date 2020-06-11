900b421f1de11f920fcdd3aa42311fef8a400fd2
Samplasion <samplasion@gmail.com>
2020-06-11 21:07:09 +0200

    Use JSON instead of YAML

--- lib/jekyll-responsive-image/block.rb
+++ lib/jekyll-responsive-image/block.rb
@@ -1,10 +1,12 @@
+require 'json'
+
 module Jekyll
   module ResponsiveImage
     class Block < Liquid::Block
       include Jekyll::ResponsiveImage::Utils
 
       def render(context)
-        attributes = YAML.load(super)
+        attributes = JSON.parse(super)
         Renderer.new(context.registers[:site], attributes).render_responsive_image
       end
     end
